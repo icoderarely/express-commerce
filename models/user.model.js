bd90135f18aa4e515ec509e0470f2843bb309c25
Navneet Anand <itworksnavneet@gmail.com>
2026-05-22 12:32:22 +0530

    add user routes

---
+++ models/user.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: { type: String, required: [true, "Name required"] },
+  email: {
+    type: String,
+    required: [true, "Email required"],
+    unique: true,
+    lowercase: true,
+  },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["user", "admin"], default: "user" },
+  address: { type: String },
+  gender: { type: String, enum: ["male", "female", "prefer not to say"] },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
